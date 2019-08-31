86900cbc957decc40e5a4ae879443751944fe732
kulmanj <preetjass2507@gmail.com>
2019-08-31 13:00:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/youtubeintegration/Config.java
@@ -0,0 +1,10 @@
+package com.example.youtubeintegration;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDDX21jRFypPiXV_g8McKUAQ0W_mv8mlJI";
+
+}
